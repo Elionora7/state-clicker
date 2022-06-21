d589388c8f26f2dc45b3913d6c77bf9ef98af812
Elionora <78257077+Elionora7@users.noreply.github.com>
2022-06-21 22:59:19 +1000

    Add all files

---
+++ src/rando.js
@@ -0,0 +1,26 @@
+import React,{Component} from "react";
+
+class Rando extends Component{
+  constructor(props){
+    super(props);
+    this.state = {num:1}
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+    handleClick() {
+    let rand = Math.floor(Math.random() * 10) + 1;
+    this.setState({num: rand});
+
+ }
+
+  render(){
+    return <div>
+        <h1>Number is: {this.state.num}</h1>
+        {(this.state.num === 7) ? (<h2>YOU WIN!</h2>) :
+        (<button onClick={this.handleClick}>Random Number</button>)
+        }
+    </div>
+  }
+}
+
+export default Rando;
